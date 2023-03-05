d7e4b73c0b067eb86f4a1e447a0fdbfa7e5a3cf6
fanhsg-lab <theofanis.giannakopoulos5@gmail.com>
2023-03-05 16:17:28 +0200

    Category item layout

--- app/src/main/java/com/example/duolingo/CategoryFragment.java
+++ app/src/main/java/com/example/duolingo/CategoryFragment.java
@@ -5,20 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CategoryFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CategoryFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
+
     private String mParam1;
     private String mParam2;
 
@@ -26,15 +25,9 @@ public class CategoryFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CategoryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+   private GridView catView;
+    private List<CategoryModel> catList = new ArrayList<>();
+
     public static CategoryFragment newInstance(String param1, String param2) {
         CategoryFragment fragment = new CategoryFragment();
         Bundle args = new Bundle();
@@ -57,6 +50,29 @@ public class CategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category, container, false);
+        View view = inflater.inflate(R.layout.fragment_category, container,false);
+
+        catView = view.findViewById(R.id.cat_Grid);
+
+        loadCategories();
+
+        CategoryAdapter adapter = new CategoryAdapter(catList);
+        catView.setAdapter(adapter);
+
+
+
+        return view;
+    }
+
+    private void loadCategories(){
+
+
+        catList.clear();
+
+        catList.add(new CategoryModel("1","GK",20));
+        catList.add(new CategoryModel("2","HISTORY",30));
+        catList.add(new CategoryModel("3","ENGLISH",10));
+        catList.add(new CategoryModel("4","SCIENCE",25));
+        catList.add(new CategoryModel("5","MATHS",20));
     }
 }

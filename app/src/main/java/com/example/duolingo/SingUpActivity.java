90cfbf4aac200456a653a681f6ae6516ae0e185a
fanhsg-lab <theofanis.giannakopoulos5@gmail.com>
2023-03-06 20:51:59 +0200

    Firebase error

--- app/src/main/java/com/example/duolingo/SingUpActivity.java
+++ app/src/main/java/com/example/duolingo/SingUpActivity.java
@@ -1,17 +1,30 @@
 package com.example.duolingo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SingUpActivity extends AppCompatActivity {
 
     private EditText name,email,pass,confirmPass;
     private Button signUpB;
     private ImageView backB;
+    private FirebaseAuth mAuth;
+    private String emailStr,passStr, confirmPassStr, nameStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,8 @@ public class SingUpActivity extends AppCompatActivity {
         signUpB = findViewById(R.id.signupB);
         backB = findViewById(R.id.backB);
 
+        mAuth= FirebaseAuth.getInstance();
+
         backB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,7 +51,77 @@ public class SingUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if(validate()) {
+                    singupNewUser();
+                }
+
             }
         });
     }
+
+    private boolean validate(){
+        nameStr=name.getText().toString().trim();
+        passStr=pass.getText().toString().trim();
+        emailStr=email.getText().toString().trim();
+        confirmPassStr=confirmPass.getText().toString().trim();
+
+        if(nameStr.isEmpty())
+        {
+            name.setError("Enter Your Name");
+            return false;
+        }
+
+        if(emailStr.isEmpty())
+        {
+            email.setError("Enter Email ID");
+        }
+
+        if(passStr.isEmpty())
+        {
+            pass.setError("Enter Password");
+        }
+
+        if(confirmPassStr.isEmpty())
+        {
+            confirmPass.setError("Enter Password");
+        }
+
+        if(passStr.compareTo(confirmPassStr)!=0)
+        {
+            Toast.makeText(SingUpActivity.this, "Passwords are not the same",Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+    return true;
+
+    }
+
+    private void singupNewUser()
+    {
+
+
+        mAuth.createUserWithEmailAndPassword(emailStr, passStr)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(SingUpActivity.this, "Sing Up Successfull ",Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(SingUpActivity.this,MainActivity.class);
+                            startActivity(intent);
+                            SingUpActivity.this.finish();
+
+
+                        } else {
+
+                           Toast.makeText(SingUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
+
 }

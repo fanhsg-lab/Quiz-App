e6762aecdc0ed46a2ee8f17b12124b4fe69e39c8
fanhsg-lab <theofanis.giannakopoulos5@gmail.com>
2023-03-14 20:12:52 +0200

    StartTestActivity / Layout in Layout

---
+++ app/src/main/java/com/example/duolingo/StartTestActivity.java
@@ -0,0 +1,14 @@
+package com.example.duolingo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class StartTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_test);
+    }
+}

e7a6ffd46cfaa7f535df1561288b0f886ca7f94e
fanhsg-lab <theofanis.giannakopoulos5@gmail.com>
2023-03-15 20:08:06 +0200

    Questions Activity not complete

--- app/src/main/java/com/example/duolingo/QuestionsActivity.java
+++ app/src/main/java/com/example/duolingo/QuestionsActivity.java
@@ -1,14 +1,48 @@
 package com.example.duolingo;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class QuestionsActivity extends AppCompatActivity {
+
+    private RecyclerView questionsView;
+    private TextView tvQuesID, timerTV, catNameTV;
+    private Button submitB, markB, clearSelB;
+    private ImageButton prevQuesB, nextQuesB;
+    private ImageView quesListB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        init();
+
+        QuestionsAdapter quesAdapter = new QuestionsAdapter(DbQuery.g_quesList);
+        questionsView.setAdapter(quesAdapter);
     }
+
+    private void init()
+    {
+        questionsView = findViewById(R.id.question_view);
+        tvQuesID = findViewById(R.id.tv_quesID);
+        timerTV = findViewById(R.id.tv_timer);
+        catNameTV = findViewById(R.id.qa_catName);
+        submitB = findViewById(R.id.submitB);
+        markB = findViewById(R.id.markB);
+        clearSelB = findViewById(R.id.clear_selB);
+        prevQuesB = findViewById(R.id.prev_quesB);
+        nextQuesB = findViewById(R.id.next_quesB);
+        quesListB = findViewById(R.id.ques_list_gridB);
+
+
+
+    }
+
 }

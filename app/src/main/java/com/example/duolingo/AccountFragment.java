961587da4cd4eea0f1143a6c6a96927ff2e8cc90
fanhsg-lab <theofanis.giannakopoulos5@gmail.com>
2023-03-17 21:28:00 +0200

    Account Layout

--- app/src/main/java/com/example/duolingo/AccountFragment.java
+++ app/src/main/java/com/example/duolingo/AccountFragment.java
@@ -2,13 +2,13 @@ package com.example.duolingo;
 
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -25,7 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class AccountFragment extends Fragment {
 
 
-    private Button logoutB;
+    private LinearLayout logoutB;
 
 
     private static final String ARG_PARAM1 = "param1";
